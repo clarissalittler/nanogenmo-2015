1eca7ff8d18ecafb80e6abb17c19e5d16dfa3e50
Clarissa Littler <clarissa.littler@gmail.com>
2015-12-20 16:50:00 -0800

    fixed some minimal markov mess

--- markov.hs
+++ markov.hs
@@ -12,15 +12,22 @@ import BibleRot
 
 type Table = Map [String] [(String, Rational)] -- invariant: the lists all must be the same length or else the algorithm doesn't work
 
+
+-- if what you're looking for doesn't show up then let's presume that we can drop down to just the
+-- last word used as a singlet instead
 nextStep :: RandomGen g => [String] -> Table -> Rand g String
-nextStep ss t = fromList $ fromJust $ (M.lookup ss t) 
+nextStep ss t = case (M.lookup ss t) of
+                  Nothing -> case (M.lookup [last ss] t) of
+                               Nothing -> error $ "howwww: " ++ (last ss)
+                               Just ws -> fromList ws
+                  Just ws -> fromList ws
 
 -- the argument for the very first call needs to be a seed, from there the further
 -- calls are generated from the results of the previous call
 generateStrings :: RandomGen g => Int -> [String] -> Table -> Rand g [String]
 generateStrings 0 _ _ = return []
 generateStrings n strs t = do 
-  s <- nextStep strs t 
+  s <- nextStep strs t
   ss <- generateStrings (n-1) (tail strs ++ [s]) t
   return $ s : ss
 
@@ -28,8 +35,9 @@ generateStrings n strs t = do
 -- parseBible returns [(String, [[String]])] which isn't quite right format. 
 -- Need to cut punctuation out and turn to all lower case as well
 toWords :: String -> [String]
-toWords =  map (map toLower) . map (filter punc) . concat . concat . map snd . parseBible
-   where punc c = and [c /= '.', c /= '!', c /= ',', c /= '"', c /= ';', c /= ','] 
+toWords = words . map toLower . filter (\x -> isAlpha x || isSpace x)
+   where punc c = and [c /= '.', c /= '!', c /= ',', c /= '"', c /= ';', c /= ',', c/= ':'] 
+         digit c = and $ map (\d -> c /= chr d) [0..9] 
 
 nGroup :: Int -> [a] -> [[a]]
 nGroup n [] = []
@@ -50,9 +58,12 @@ groupsToFrequencies :: (Ord a , Eq a) => [[a]] -> Map [a] [(a, Rational)]
 groupsToFrequencies ts = let tGroups = nub $ map dropLast ts
                          in M.fromList $ map (\ x -> (x, prefixToFrequencies x ts)) tGroups
 
-verseGenerator :: Int -> IO String
-verseGenerator n = do
-  s <- readFile "kjv10.txt"
+verseGenerator :: String -> Int -> IO ()
+verseGenerator f n = do
+  s <- readFile f
   let strs = toWords $ take 1000000 s
-      tab = groupsToFrequencies $ nGroup 3 strs
-  liftM concat $ evalRandIO $ generateStrings n ["the", "lord"] tab
+      tab1 = groupsToFrequencies $ nGroup 3 strs
+      tab2 = groupsToFrequencies $ nGroup 2 strs
+      tab = M.union tab1 tab2
+  x <- liftM unwords $ evalRandIO $ generateStrings n ["the", "lord"] tab
+  putStrLn $ "the lord " ++ x
